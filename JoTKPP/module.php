23ddbaff29580b8c49c42fcbd6a2812b9db1b122
jotata <admin@tanner-info.ch>
2021-01-03 14:29:04 +0100

    Added Access column in form

--- JoTKPP/module.php
+++ JoTKPP/module.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * @File:            module.php
  * @Create Date:     09.07.2020 16:54:15
  * @Author:          Jonathan Tanner - admin@tanner-info.ch
- * @Last Modified:   02.01.2021 10:25:00
+ * @Last Modified:   03.01.2021 14:22:00
  * @Modified By:     Jonathan Tanner
  * @Copyright:       Copyright(c) 2020 by JoT Tanner
  * @License:         Creative Commons Attribution Non Commercial Share Alike 4.0
@@ -178,6 +178,13 @@ class JoTKPP extends JoTModBus {
             $variable['cName'] = '';
             $variable['Profile'] = $this->CheckProfileName($config['Profile']); //Damit wird der PREFIX immer davor hinzugefügt
             $variable['cProfile'] = '';
+            $variable['Access'] = '';
+            if (array_key_exists('RFunction', $config) || $config['Address'] === 0) {
+                $variable['Access'] .= 'R';
+            }
+            if (array_key_exists('WFunction', $config)) {
+                $variable['Access'] .= 'W';
+            }
             $variable['FWVersion'] = floatval($config['FWVersion']);
             $variable['Poll'] = false;
             if (strpos(" $pollIdents ", " $ident ") !== false) {//Aktivierte Idents markieren und Gruppe öffnen
